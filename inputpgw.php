80f275a0827c81e0067f95333be2f0172af4903e
aldipriyanta <22083000074@student.unmer.ac.id>
2025-07-08 22:44:43 +0700

    menyamakan input

--- inputpgw.php
+++ inputpgw.php
@@ -18,8 +18,29 @@
             $response['message'] = 'New record created successfully';
         } else {
             $response['status'] = 'error';
-            $response['message'] = 'Error: ' . $sql . '<br>' . $conn->error;
+            $response['message'] = 'Error:' . mysqli_error($conn);    
         }
+
         return $response;
     }
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(isset($_POST['id_pegawai']) && isset($_POST['nama']) && isset($_POST['jabatan']) && isset($_POST['status_kepegawaian']) && isset($_POST['gaji_pokok'])) {
+        $id_pegawai = $_POST['id_pegawai'];
+        $nama = $_POST['nama'];
+        $jabatan = $_POST['jabatan'];
+        $status_kepegawaian = $_POST['status_kepegawaian'];
+        $gaji_pokok = $_POST['gaji_pokok'];
+
+        $result = inputPegawai($id_pegawai, $nama, $jabatan, $status_kepegawaian, $gaji_pokok, $conn);
+    } else {
+        $result = array(
+            'status' => 'error',
+            'message' => 'Data input tidak lengkap'
+        );
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($result);
+}
 ?>
